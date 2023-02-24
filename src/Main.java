e1acfc75e2c577b941d8f1c2fe398c2b07c174ae
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-02-24 15:33:47 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {// loop that runs from 5 to 1 in descending order)
+        for (int i = 5; i >= 1; i--) {// loop that runs from 1 to i
+            for (int j = 1; j <= i; j++) {// Print a single asterisk without a newline character
+                System.out.print("*");
+            }// After the inner loop has finished, print a newline character to move to the next line
+            System.out.println();
+        }
     }
 }
